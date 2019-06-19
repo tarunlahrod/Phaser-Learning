4007019b86bc2e64dee79a458532f67a722c5492
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-06-19 18:47:25 +0530

    Learning started

---
+++ main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
+
+var mainState = {
+	preload:function(){
+
+	},
+
+	create:function(){
+
+	},
+
+	update:function(){
+
+	}
+}
+
+game.state.add('mainState', mainState);
+game.state.start('mainState');
